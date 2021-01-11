9aeceb71e6dbd524a49845506c9fbc9ec3c65cdd
lucas-montenegro <lucasmaassuncao@gmail.com>
2021-01-11 16:07:15 -0200

    Inserting movies data base and updating main

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from infeng import Engine
 
-eng = Engine('knowledge_base.json')
+def asking(value):
+    while True:
+        i = input(value.description).lower()
+        if i == 'sim':
+            value.value = True
+            return
+        elif i == 'nao':
+            value.value = False
+            return
+        else:
+            print('Resposta inválida. Tente novamente.')
+
+
+eng = Engine('knowledge_base.json', asking)
+
+print('Bem vindo ao bot de recomendação de filmes!')
+print('Você deverá responder as perguntas com um sim (caso afirmativo) ou nao (caso falso)')
+print('Iniciando avaliação...')
+
+
 
 while True:
-    print('-> ',eng.evaluate(input('>>> ')))
+    if(eng.evaluate('Lista1') == True):
+        print('Comedia 1, 2 e 3')
+        break
+    else:
+        print('Esses filmes não são para você!')
+        break
