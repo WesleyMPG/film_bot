97abd0aa6a7973e2510d24de415ccb40d45ab304
wesley <wesleydos504@gmail.com>
2021-01-25 02:20:46 -0300

    extensão

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import re
 from infeng import Engine
 
 def asking(value):
     while True:
-        i = input(value.description).lower()
+        i = input(value.description+' ').lower()
         if i == 'sim':
             value.value = True
-            return
+            break
         elif i == 'nao':
             value.value = False
+            break
+        else:
+            print('Resposta inválida. Tente novamente.')
+    while True:
+        c = input(f'Quão certo você está disso (%)? ').lower()
+        found = re.match(r'^\d{1,2}([\.,]\d+)?$|^100$', c)
+        if found is not None:
+            if ',' in c:
+                c = c.replace(',', '.')
+            value.confidence = float(c)
             return
         else:
             print('Resposta inválida. Tente novamente.')
@@ -24,49 +35,62 @@ print('Iniciando avaliação...')
 print('---------------------------------------------------------------------')
 
 
-while True:
-    if(eng.evaluate('Lista1') == True):
+def main():
+    result, confidence = eng.evaluate('Lista1')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l100')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L01')
-        break
-    elif(eng.evaluate('Lista2') == True):
+        return
+    result, confidence = eng.evaluate('Lista2')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l200')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L02')
-        break
-    elif(eng.evaluate('Lista3') == True):
+        return
+    result, confidence = eng.evaluate('Lista3')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l300')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L03')
-        break
-    elif(eng.evaluate('Lista4') == True):
+        return
+    result, confidence = eng.evaluate('Lista4')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l400')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L04')
-        break
-    elif(eng.evaluate('Lista5') == True):
+        return
+    result, confidence = eng.evaluate('Lista5')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l500')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L05')
-        break
-    elif(eng.evaluate('Lista6') == True):
+        return
+    result, confidence = eng.evaluate('Lista6')
+    if(result == True):
         print('---------------------------------------------------------------------')
         print('PREPARAMOS UMA LISTA COM FILMES SELECIONADOS PARA VOCÊ ASSISTIR HOJE!')
         print('ACESSE ESSE LINK E CONFIRA --> https://cutt.ly/recomendacao_de_filmes_0l600')
         print('---------------------------------------------------------------------')
+        print(f'\nNível de confiaça: {confidence}\n')
         print('CÓDIGO DA OPERAÇÃO: L06')
-        break
-    else:
-        print('Esses filmes não são para você!')
-        break
+        return
+    print('Esses filmes não são para você!')
+
+
+main()
